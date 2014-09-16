791539ef5b02fe48d6c5268f5de8f96274f3332d
Jagdish Barabari <jagdish151947@gmail.com>
2014-09-17 00:33:35 +0530

    initial commint

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bubai::Application.config.secret_token = '4abee36f20cb7ff86b630bbdabb4d87b4b5b9649daaeb85614eb936ffb624d442adc9409585d1ceb5fb6f286687f466cf2593ad7f7140fd4d118daf59a11e905'
