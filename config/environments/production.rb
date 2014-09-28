5c26b955bcdf114be77456a1c1075bcaed81671e
Jagdish Barabari <jagdish151947@gmail.com>
2014-09-29 02:14:28 +0530

    Assets precompiled

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Bubai::Application.configure do
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
 
-  
+  config.assets.precompile += %w( mercury.js mercury.css mercury_overrides.css )
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
